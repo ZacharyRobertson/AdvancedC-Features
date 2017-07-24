f584edd908a82f85ff7b97ac36d6a93e9be183ac
Zachary Robertson <ztrsec@hotmail.com>
2017-07-24 16:02:56 +1000

    Adding Scripts

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class CustomList<T>
+    {
+        public T[] list;
+        public int amount { get; private set; }
+
+        // Default constructor
+        public CustomList() { amount = 0; }
+        // gameObjects[0]
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+        // Create an Add function
+        // Adds an item to the end of the list
+        public void Add(T item)
+        {
+            // Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            // Check if the list has been initialized
+            if (list != null)
+            {
+                // Copy all existing items to the new array
+                for (int i = 0; i < list.Length; i++)
+                {
+                    cache[i] = list[i];
+                }
+            }
+            // Place new item at end of index
+            cache[amount] = item;
+            // Replace old array with new array
+            list = cache;
+            // Increment Amount
+            amount++;
+        }
+    }
+}
---
+++ Assets/1-Generics/Scripts/GenericsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class GenericsTest : MonoBehaviour
+    {
+        public GameObject prefab;
+        public int spawnAmount = 20;
+        public float spawnRadius = 10f;
+        public CustomList<GameObject> gameObjects = new CustomList<GameObject>();
+        // Use this for initialization
+        void Start()
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                GameObject clone = Instantiate(prefab);
+                Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius;
+                clone.transform.position = randomPos;
+                gameObjects.Add(clone);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
