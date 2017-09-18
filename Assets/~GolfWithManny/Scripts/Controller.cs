6aca0b40abd6e42f752a68654caa8e5b77882513
ZacharyRobertson <ztrsec@hotmail.com>
2017-09-18 14:00:19 +1000

    Adding Global Camera Script: DisownSelfAtStart and uploading ~GolfWithManny

---
+++ Assets/~GolfWithManny/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GolfWithManny
+{
+    [RequireComponent(typeof(Player))]
+    public class Controller : MonoBehaviour
+    {
+        public Camera cam;
+        private Player p;
+
+        // Use this for initialization
+        void Start()
+        {
+            p = GetComponent<Player>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.W))
+            {
+                Vector3 forward = cam.transform.forward;
+                p.Move(forward);
+            }
+        }
+    }
+}
---
+++ Assets/~GolfWithManny/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GolfWithManny
+{
+    public class Player : MonoBehaviour
+    {
+        public float speed = 50f;
+        public float maxVelocity = 20f;
+
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Awake()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            Vector3 vel = rigid.velocity;
+            if(vel.magnitude > maxVelocity)
+            {
+                vel = vel.normalized * maxVelocity;
+            }
+            rigid.velocity = vel;
+        }
+
+        public void Move(Vector3 direction)
+        {
+            rigid.AddForce(direction * speed, ForceMode.Impulse);
+        }
+
+        
+    }
+}
