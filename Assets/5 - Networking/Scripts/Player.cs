bef4ebb982834051504df2a5498c041201858a43
Zachary Robertson <ztrsec@hotmail.com>
2017-10-09 15:26:51 +1100

    Uploading LocalUser and player Script in Networking project

---
+++ Assets/5 - Networking/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Networking
+{
+    public class Player : MonoBehaviour
+    {
+        public float moveSpeed = 8f;
+        public float rotationSpeed = 360f;
+        public float jumpHeight = 10.0f;
+
+        private bool isGrounded = false;
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+        public void Move(float vertical, float horizontal)
+        {
+            Vector3 position = rigid.position;
+            Quaternion rotation = rigid.rotation;
+
+            position += transform.forward * vertical * moveSpeed * Time.deltaTime;
+            rotation *= Quaternion.AngleAxis(rotationSpeed * horizontal * Time.deltaTime, Vector3.up);
+
+            rigid.MovePosition(position);
+            rigid.MoveRotation(rotation);
+        }
+        
+        public void Jump()
+        {
+            if(isGrounded)
+            {
+                rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+        void OnCollisionEnter(Collision other)
+        {
+            isGrounded = true;
+        }
+    }
+}
